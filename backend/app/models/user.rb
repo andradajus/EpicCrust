58b48cf6824a1923d94af379021796c2a608c594
andradajus <138541478+andradajus@users.noreply.github.com>
2024-02-23 23:46:03 +0800

    Created initial devise authentication setup for api

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,7 +1,14 @@
 class User < ApplicationRecord
-    devise :database_authenticatable,
-           :jwt_authenticatable, jwt_revocation_strategy: Denylist
-
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+  :confirmable,
+  :registerable,
+  :recoverable,
+  :rememberable,
+  :validatable,
+  :jwt_authenticatable,
+  jwt_revocation_strategy: self
 
 
   has_many :orders
