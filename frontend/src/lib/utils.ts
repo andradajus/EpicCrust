afe00a4bd61aa98c6dad2e5cb847ddd324e67925
andradajus <138541478+andradajus@users.noreply.github.com>
2024-02-24 19:33:25 +0800

    Integrated backend registration API to frontend

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,8 +1,37 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import axios from "axios";
+
+const backendBaseUrl = import.meta.env.VITE_BACKEND_BASE_URL;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const registerUser = async (event: React.FormEvent<HTMLFormElement>) => {
+  event.preventDefault();
 
+  const formData = new FormData(event.currentTarget);
+  const newUser = {
+    user: {
+      first_name: formData.get("first_name"),
+      middle_name: formData.get("middle_name"),
+      last_name: formData.get("last_name"),
+      password: formData.get("password"),
+      email: formData.get("email"),
+      address: formData.get("address"),
+      mobile_number: formData.get("mobile_number"),
+    },
+  };
+  console.log("Registration newUser", newUser)
+  try {
+    const res = await axios.post(`${backendBaseUrl}/users`, newUser);
+    return res;
+  } catch (error) {
+    if (error) {
+      return error;
+    } else {
+      return error;
+    }
+  }
+};
